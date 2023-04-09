405be8866ea64dfdb90f1ea9585ab73b4e5e1686
Silko9 <silvanillusive@gmail.com>
2023-04-09 12:55:39 +0300

    основа для добавление аунтификации

---
+++ Areas/Identity/Data/MusicShopUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace musicShop.Areas.Identity.Data
+{
+    public class MusicShopUser : IdentityUser
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,12 +1,35 @@
 using Microsoft.EntityFrameworkCore;
 using musicShop.Models;
+using Microsoft.AspNetCore.Identity;
+using musicShop.Data;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using musicShop.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages().AddRazorRuntimeCompilation();
 builder.Services.AddDbContext<AppDbContext>(options =>
  options.UseSqlServer(builder.Configuration.GetConnectionString("AppData")));
+
+builder.Services.AddDbContext<musicShopContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("AppData")));
+
+builder.Services.AddIdentity<MusicShopUser, IdentityRole>(options =>
+{
+    options.SignIn.RequireConfirmedAccount = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequiredLength = 4;
+}).
+    AddEntityFrameworkStores<musicShopContext>();
+
+builder.Services.ConfigureApplicationCookie(opt =>
+{
+    opt.AccessDeniedPath = new PathString("/Identity/Account/AccessDenied");
+    opt.LoginPath = new PathString("/Identity/Account/Login");
+});
 
 var app = builder.Build();
 
@@ -23,10 +46,13 @@ app.UseStaticFiles();
 
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.MapRazorPages();
 
 app.Run();
